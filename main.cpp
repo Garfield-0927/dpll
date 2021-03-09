a4a11b6124ec6208d878e8f7e3cd62f9913ff724
Garfield_zhang <929780650@qq.com>
2021-03-09 14:21:52 +0800

    final

--- main.cpp
+++ main.cpp
@@ -11,9 +11,12 @@ int main() {
     while (option){
         cin>>option;
         if (option == 1){
-            PuzzleToCnf("sudoku1.txt", "sudoku1.cnf");
-            string filename = "sudoku1.cnf";
-//            cin>>filename;
+            cout<<"please input the filename of sudoku:";
+            string sudokufile;
+            cin >> sudokufile;
+
+            PuzzleToCnf(sudokufile+".txt", sudokufile+".cnf");
+            string filename = sudokufile+".cnf";
 
             int VARNUM = 0;
             ClauseNode * ClauseHead = nullptr;
@@ -49,12 +52,14 @@ int main() {
 
 
         }else if (option == 2){
-            string filename = "sud00021.cnf";
-//            cin>>filename;
+            cout<<"please input the filename of cnf:";
+
+            string filename;
+            cin>>filename;
 
             int VARNUM = 0;
             ClauseNode * ClauseHead = nullptr;
-            ClauseHead = openCnf(filename, VARNUM);
+            ClauseHead = openCnf(filename+".cnf", VARNUM);
             int * res = (int*)malloc(VARNUM*sizeof(int));
             memset(res,-1,sizeof(res));
             for (int j = 0; j < VARNUM; ++j) {
@@ -68,11 +73,10 @@ int main() {
             if (value){
                 cout << "S  1" << endl;
                 for (int i = 0; i < VARNUM; ++i) {
-                    if (res[i] == -1){
-                        cout << "1 ";
-                    } else {
-                        cout << res[i] << " ";
+                    if (res[i] == -1) {
+                        res[i] = 1;
                     }
+                    cout<<res[i]<<" ";
                 }
                 cout<<endl;
             } else {
@@ -81,7 +85,24 @@ int main() {
 
             cout<<"T "<<(double)(end-start)/CLOCKS_PER_SECOND *1000<<" ms\n";
 
-
+            string outputPrefix = "D:\\garfield\\study\\data structure\\dpll\\res\\";
+            string outpath;
+            outpath = outputPrefix.append(filename+".res");
+            ofstream myRes(outpath);
+            if (value){
+                myRes<<"S 1"<<endl;
+                for (int i = 0; i < VARNUM; ++i) {
+                    if (res[i]==1){
+                        myRes<<(i+1)<<" ";
+                    } else{
+                        myRes<<-(i+1)<<" ";
+                    }
+                }
+            } else{
+                myRes<<"S 0"<<endl;
+            }
+            myRes<<endl<<(double)(end-start)/CLOCKS_PER_SECOND *1000<<" ms\n";
+            myRes.close();
         }else if (!option){
             break;
         }else{
